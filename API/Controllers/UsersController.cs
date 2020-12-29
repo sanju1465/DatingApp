2dcf379fef829e5941b6f014904930adb7133921
SanjayDhara <sanjay.dhara@bandhan.org>
2020-12-29 14:26:21 +0530

    Initail Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // [HttpGet]
+        // public ActionResult<IEnumerable<AppUser>> GetUsers()
+        // {
+        //     return _context.Users.ToList();
+        // }
+
+        // [HttpGet("{id}")]
+        // public ActionResult<AppUser> GetUser(int id)
+        // {
+        //     return _context.Users.Find(id);            
+        // }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);            
+        }
+    }
+}
