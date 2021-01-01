54a3f5ba39a1acce644b32ec7a2c044f12aa5369
SanjayDhara <sanjay.dhara@bandhan.org>
2021-01-01 15:42:37 +0530

    Registered and Login in client

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,15 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  model: any = {}
+  model: any = {}  
 
-  constructor() { }
+  constructor(public accountService: AccountService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
   }
 
   login(){
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe(response =>{
+      console.log(response);      
+    }, error =>{
+      console.log(error);
+    })
   }
 
+  logout(){
+    this.accountService.logout();    
+  }
 }
