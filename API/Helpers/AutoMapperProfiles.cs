381b1d3e5ff71b58570bdc1b3ebaf856d2656495
SanjayDhara <sanjay.dhara@bandhan.org>
2021-01-08 17:20:42 +0530

    Update member Loading Caching data

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -16,6 +16,7 @@ namespace API.Helpers
                 .ForMember(dest => dest.Age, op => op.MapFrom(src =>
                     src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotoDto>();
+            CreateMap<MemberUpdateDto, AppUser>();
         }
     }
 }
